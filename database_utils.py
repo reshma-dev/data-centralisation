6a1c04c0979083e725af22fd045f6f28ed0e7f58
reshma-dev <reshma.sahasrabudhe@gmail.com>
2023-11-21 23:21:52 +0000

    - update db connection methods to use creds filename - add method to upload DataFrame to DB

--- database_utils.py
+++ database_utils.py
@@ -2,9 +2,14 @@ class DatabaseConnector:
     """
     Class to connect to database
     """
-    def read_db_creds(self):
+    def read_db_creds(self, creds_file_name):
         """
         Read db_creds.yaml file containing the database credentials and return a dictionary of the credentials
+
+        Parameters: 
+        ----------
+        creds_file_name: string
+            Filename of the file to read DB Credentials from
 
         Returns:
         --------
@@ -12,20 +17,25 @@ class DatabaseConnector:
             A dictionary containing credentials read from db_creds.yaml
         """
         import yaml
-        with open('db_creds.yaml', 'r') as file:
+        with open(creds_file_name, 'r') as file:
             return yaml.safe_load(file)
         
 
-    def init_db_engine(self):
+    def init_db_engine(self, creds_file_name):
         """
         Read database credentials from the credentials file and initialise and return a sqlalchemy database engine
         
+        Parameters: 
+        ----------
+        creds_file_name: string
+            Filename of the file to read DB Credentials from
+
         Returns:
         --------
         <class 'sqlalchemy.engine.base.Engine'>
             Initialised instance of the SQLAlchemy Database Engine object
         """
-        db_creds = self.read_db_creds()
+        db_creds = self.read_db_creds(creds_file_name)
 
         from sqlalchemy import create_engine
 
@@ -48,13 +58,38 @@ class DatabaseConnector:
         list
             List of all the tables in the connected database
         """
-        engine = self.init_db_engine()
+        engine = self.init_db_engine('db_creds.yaml')
 
         with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
             from sqlalchemy import inspect
             inspector = inspect(engine)
             return inspector.get_table_names()
+        
+    def upload_to_db(self, df, table_name):
+        """
+        This method will upload a Pandas DataFrame to a table.
+
+        Parameter:
+        df: <class 'pandas.core.frame.DataFrame'>
+            The DataFrame from which data will be uploaded
+
+        table_name: string
+            The RDS table to which the data from DataFrame will be uploaded 
+        """
+        engine = self.init_db_engine('db_creds_local.yaml')
+
+        with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
+            df.to_sql(table_name, conn, if_exists= 'replace')
+
 
 if __name__ == "__main__":
-    dbc = DatabaseConnector()
-    print(dbc.list_db_tables())
+    dbconn = DatabaseConnector()
+    print(dbconn.list_db_tables())
+
+    from data_cleaning import DataCleaning
+
+    dbclean = DataCleaning()
+    df = dbclean.clean_user_data()
+    print(f"Number of clean entries: {len(df)}")
+
+    dbconn.upload_to_db(df, 'dim_users')
