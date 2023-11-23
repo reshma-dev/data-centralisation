e5ef2886dc07b1da38406f5b81e1f9f6d3b79bda
reshma-dev <reshma.sahasrabudhe@gmail.com>
2023-11-23 08:35:57 +0000

    add methods to extract stores data using API

--- data_extraction.py
+++ data_extraction.py
@@ -45,6 +45,67 @@ class DataExtractor:
         import tabula
 
         return tabula.read_pdf(pdf_path, pages='all')
+    
+    def __read_api_creds(self, creds_file_name):
+        """
+        Read api_creds.yaml file containing the api key
+
+        Parameters: 
+        ----------
+        creds_file_name: string
+            Filename of the file to read DB Credentials from
+
+        Returns:
+        --------
+        <class 'dict'>
+            A dictionary containing credentials read from db_creds.yaml
+        """
+        import yaml
+        with open(creds_file_name, 'r') as file:
+            return yaml.safe_load(file)
+    
+    def list_number_of_stores(self, stores_count_url:str):
+        """
+        Method to get the total number of stores
+        """
+        import requests
+        header_details = self.__read_api_creds('api_creds.yaml')
+        r = requests.get(stores_count_url, headers=header_details)
+        return r.json()['number_stores']
+    
+    def retrieve_store_data(self, store_by_number_url:str):
+        """
+        Method to retrieve store details for the given store number
+        """
+        import requests
+        header_details = self.__read_api_creds('api_creds.yaml')
+        return requests.get(store_by_number_url, headers=header_details)
+    
+    def get_stores(self):
+        """
+        Method to fetch the number of stores and then get the data for each store
+        The data is collected in a list and then returned as a DataFrame
+        """
+        import json
+        import pandas as pd
+        
+        store_by_number_url = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}"
+        stores_count_url = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
+        
+        num_stores = self.list_number_of_stores(stores_count_url)
+
+        stores = []
+
+        for i in range(0, num_stores):  
+            response = self.retrieve_store_data(store_by_number_url.replace('{store_number}', str(i)))
+
+            if response.status_code == 200:
+                stores.append(json.loads(response.content.decode()))
+            else:
+                print(f"Error for request {i}: {response.status_code}")
+        
+        return pd.json_normalize(stores)
+    
 
 if __name__ == "__main__":
     dbe = DataExtractor()
@@ -52,5 +113,8 @@ if __name__ == "__main__":
     # df = dbe.read_rds_table(dbc, 'legacy_users')
     # print(df.head())
 
-    link_to_pdf = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
-    print(dbe.retrieve_pdf_data(link_to_pdf))
+    # link_to_pdf = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+    # print(dbe.retrieve_pdf_data(link_to_pdf))
+
+    df = dbe.get_stores()
+    print(df.head())
